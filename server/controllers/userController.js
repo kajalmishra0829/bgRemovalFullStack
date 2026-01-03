670620213694c95e40ab16688d8f6c8989cdaa05
KajalMishra-29 <kajalmishra0829@gmail.com>
2026-01-04 02:56:27 +0530

    webhooks added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ import userModel from '../models/user.js'
 // API contrller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 
-const clerkWebhooks = async (requestAnimationFrame, res) => {
+const clerkWebhooks = async (req, res) => {
     try {
         // create a svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
