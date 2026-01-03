22b5b52fc17fcb3b77efde8838825dda8e6512e3
KajalMishra-29 <kajalmishra0829@gmail.com>
2026-01-04 02:45:02 +0530

    webhooks added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,7 +20,7 @@ const clerkWebhooks = async (requestAnimationFrame, res) => {
         switch (type) {
             case "user.created": {
                 const userData = {
-                    clerkID: data.id,
+                    clerkId: data.id,
                     email: data.email_addresses[0].email_address,
                     firstName: data.first_name,
                     lastName: data.last_name,
@@ -39,14 +39,14 @@ const clerkWebhooks = async (requestAnimationFrame, res) => {
                     lastName: data.last_name,
                     photo: data.image_url
                 }
-                await userModel.findOneAndUpdate({ clerkID: data.id }, userData)
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
                 res.json({})
 
                 break;
             }
 
             case "user.deleted": {
-                await userModel.findOneAndDelete({ clerkID: data.id })
+                await userModel.findOneAndDelete({ clerkId: data.id })
                 res.json({})
 
                 break;
